1c451d51226379be6bb71731a6fbc44ec864f135
Thang Le <lethang098194@gmail.com>
2020-04-19 13:34:33 +0700

    Activity4

---
+++ src/lecture1/Activity4.java
@@ -0,0 +1,10 @@
+package lecture1;
+import java.util.Scanner;
+public class Activity4 {
+    public static void main(String [] args){
+        int a = 8;
+        for (int i = 1; i<=10; i++){
+        System.out.println("Kết quả: " + a + " x " +  i  + " = " + a * i);
+        }
+    }
+}
