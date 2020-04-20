577d1e6283952c13f0ec43d9d2f04825472fdf6e
Thang Le <lethang098194@gmail.com>
2020-04-21 01:02:24 +0700

    bảng cửu chương

---
+++ src/lecture1/bangcuachuong.java
@@ -0,0 +1,11 @@
+package lecture1;
+
+public class bangcuachuong {
+    public static void main(String[] args) {
+        for (int a = 1; a<=10 ; a++){
+            for(int i = 1; i<=10; i++){
+                System.out.println("Kết quả: " + a + " x " +  i  + " = " + a * i);
+            }
+        }
+    }
+}
